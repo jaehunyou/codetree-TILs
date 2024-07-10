51369ad98081222926053541e5d44b1b4ee681af
jaehunyou <100469084+jaehunyou@users.noreply.github.com>
2024-07-10 12:18:17 +0900

    Title: 실수 받아 그대로 출력, Time: 137ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240710/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        // 추가 코드 작성
+
+        double n = sc.nextDouble();
+
+        System.out.printf("%.2f", n);
+
+    }
+}
