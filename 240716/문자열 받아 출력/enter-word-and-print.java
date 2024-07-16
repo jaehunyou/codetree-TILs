3684516f194d574c615d223c8a005afcc3ffc37b
jaehunyou <100469084+jaehunyou@users.noreply.github.com>
2024-07-16 21:27:37 +0900

    Title: 문자열 받아 출력, Time: 114ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240716/문자열 받아 출력/enter-word-and-print.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String c = sc.next(); // 입력받은 문자열의 첫 번째 문자 추출
+        System.out.println(c);
+    }
+}
