36c5bf6f87d6a43c85d338568f72dc0150f6f749
jaehunyou <100469084+jaehunyou@users.noreply.github.com>
2024-10-19 19:37:40 +0900

    Title: 방향에 맞춰 이동, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 241019/방향에 맞춰 이동/move-in-direction.py
+++ 241019/방향에 맞춰 이동/move-in-direction.py
@@ -1,7 +1,32 @@
-dir_num = 2
-x, y = 1, 5
+#N, S, E, W == 0, 1, 2, 3
 
-dx = [1, 0, -1, 0]
-dy = [0, -1, 0, 1]
+n = int(input())
+x, y = 0, 0
 
-nx, ny = x + dx[dir_num], y + dy[dir_num]
+for i in range (n):
+    c_dir, dist = tuple(input().split())
+    dist = int(dist)
+
+    dir_map = {
+        'E' : 0,
+        'S' : 1,
+        'W' : 2,
+        'N' : 3
+    }
+    dir_num = dir_map[c_dir]
+
+    
+    ''' if c_dir == 'E':
+        dir_num = 0
+    elif c_dir == 'S':
+        dir_num = 1
+    elif c_dir == "W":
+        dir_num = 2
+    elif c_dir == 'N':
+        dir_num = 3 '''
+    
+    dx = [1, 0, -1, 0]
+    dy = [0, -1, 0, 1]
+    #dir_num 방향으로 dist 만큼 이동한 최종 위치
+    x, y = x + dx[dir_num] * dist, y + dy[dir_num] * dist
+print(x,y)
